28deae7c26d9df49193e835d02e3093a824af3a7
Andrei Borisov <andrey.igorevich.borisov@gmail.com>
2020-09-24 17:18:35 -0400

    Update 08-is-equal-deep.js

--- objects/solutions/08-is-equal-deep.js
+++ objects/solutions/08-is-equal-deep.js
@@ -1,8 +1,8 @@
 /**
  * Task description: Write a method that makes a deep compare of two objects
  * Expected Result: True if objects are equal, false if objects are different ({ a: 1, b: { c: 1 } }, { a: 1, b: { c: 1 } }) => true
- * @param {Object<string | number>} firstObj - Object with values of primitive data types
- * @param {Object<string | number>} secondObj - Object with values of primitive data types
+ * @param {Object} firstObj - Object of any values
+ * @param {Object} secondObj - Object of any values
  * @returns {boolean}
  */
 export const isEqualDeep = (firstObj, secondObj) => {
